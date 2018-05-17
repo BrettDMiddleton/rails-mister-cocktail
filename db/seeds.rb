e79be5e403ff3afc2d90a6b1e770ab4eb6c20bbf
Brett Middleton <brett01101101@gmail.com>
2018-05-17 12:36:55 -0400

    lunchtime

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require "rest-client"
+require 'json'
+
+
+url = RestClient.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+seed_result = JSON.parse(url)
+
+seed_result["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.new(
+    name: ingredient["strIngredient1"]
+    )
+  new_ingredient.save!
+end
